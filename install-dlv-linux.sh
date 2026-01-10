bab58d2c83400cad4bd3f9a3ef095d0018f7fdc0
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2026-01-10 20:53:45 +0100

    Updated DLV

--- install-dlv-linux.sh
+++ install-dlv-linux.sh
@@ -1,24 +1,28 @@
 # Install dependency if not present (Debian/Ubuntu)
 # sudo apt-get update && sudo apt-get install -y unixodbc
 
+# If on MacOS, exit with message
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "This script is intended for Linux systems only. Please follow the instructions in the README.md for MacOS."
+    exit 1
+fi
+
 CONFIG_FILE="configs/asp_generator/config.yaml"
 
-# Check if already installed
+# Check if already installed on linux
 if command -v dlv &> /dev/null
 then
     echo "DLV is already installed at $(command -v dlv). Skipping installation."
-    echo "Here's the config file:"
-    cat "$CONFIG_FILE"
-    exit 0
+else
+    echo "DLV not found. Proceeding with installation."
+
+    DLV_URL="https://www.dlvsystem.it/files/dlv.x86-64-linux-elf-unixodbc"
+    curl -Lo dlv $DLV_URL
+    chmod +x dlv
 fi
 
-echo "Downloading and installing DLV..."
-
-DLV_URL="https://www.dlvsystem.it/files/dlv.x86-64-linux-elf-unixodbc"
-curl -Lo dlv $DLV_URL
-chmod +x dlv
-
-DLV_PATH="$(pwd)/dlv"
+DLV_PATH="$(command -v dlv || echo "$PWD/dlv")"
+echo "Updating DLV path in $CONFIG_FILE to $DLV_PATH"
 
 # Using '@' as a delimiter to avoid issues with '/' or '|' in paths
 sed -i.bak "s@^dlv: .*@dlv: $DLV_PATH@" "$CONFIG_FILE"
@@ -26,5 +30,5 @@ rm "$CONFIG_FILE.bak"
 
 echo "DLV path updated to: $DLV_PATH"
 
-echo "Here's the config file:"
+echo "Here's the config file (please verify):"
 cat "$CONFIG_FILE"
