9afdceefa2275c7d20b38bde5721b1b0ba7a9ba7
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2026-01-10 21:14:34 +0100

    Update to Docker container

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -4,7 +4,11 @@
 # uv is installed in Dockerfile, just sync
 uv sync --dev
 
-./install-dlv-linux.sh
+# Update DLV path in config (DLV is installed in Dockerfile)
+CONFIG_FILE="configs/asp_generator/config.yaml"
+if [ -f "$CONFIG_FILE" ]; then
+    sed -i 's@dlv: .*@dlv: /usr/local/bin/dlv@' "$CONFIG_FILE"
+fi
 
 # Install pre-commit hooks
 uv run pre-commit install --install-hooks
