6155795589d4adca535adae380bc303152ec3951
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2026-01-10 21:11:08 +0100

    Update devcontainer test

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -4,9 +4,7 @@
 # uv is installed in Dockerfile, just sync
 uv sync --dev
 
-# Update DLV path in config (DLV is installed in Dockerfile)
-CONFIG_FILE="configs/asp_generator/config.yaml"
-sed -i 's@dlv: .*@dlv: /usr/local/bin/dlv@' "$CONFIG_FILE"
+./install-dlv-linux.sh
 
 # Install pre-commit hooks
 uv run pre-commit install --install-hooks
