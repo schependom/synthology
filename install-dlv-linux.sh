7dbeb5ac3aa20381d69959b306e56d8c16710f7f
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2026-01-10 20:50:16 +0100

    Updated DLV installation #2

--- install-dlv-linux.sh
+++ install-dlv-linux.sh
@@ -1,6 +1,8 @@
 # Install dependency if not present (Debian/Ubuntu)
 # sudo apt-get update && sudo apt-get install -y unixodbc
 
+CONFIG_FILE="configs/asp_generator/config.yaml"
+
 # Check if already installed
 if command -v dlv &> /dev/null
 then
@@ -16,7 +18,6 @@ DLV_URL="https://www.dlvsystem.it/files/dlv.x86-64-linux-elf-unixodbc"
 curl -Lo dlv $DLV_URL
 chmod +x dlv
 
-CONFIG_FILE="configs/asp_generator/config.yaml"
 DLV_PATH="$(pwd)/dlv"
 
 # Using '@' as a delimiter to avoid issues with '/' or '|' in paths
